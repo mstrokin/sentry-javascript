c34e48cb320f8078c61c379e58c1d87f00cfefbe
Francesco Novy <francesco.novy@sentry.io>
2024-12-30 16:42:27 +0100

    fix hub shim??

--- packages/core/src/getCurrentHubShim.ts
+++ packages/core/src/getCurrentHubShim.ts
@@ -83,7 +83,7 @@ export const getCurrentHub = getCurrentHubShim;
  * Sends the current Session on the scope
  */
 function _sendSessionUpdate(): void {
-  const scope = getCurrentScope();
+  const scope = getIsolationScope();
   const client = getClient();
 
   const session = scope.getSession();
